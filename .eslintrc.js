109e48cdf3ff6fd4cf54637a3d70447fd188af99
David Sillman <dsillman2000@gmail.com>
2026-02-08 16:10:21 -0500

    fix: track .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  env: {
+    node: true,
+    es2020: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'quote-props': ['error', 'as-needed'],
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'coverage/', '*.js'],
+};
